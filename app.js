2c29ffc4369d19fb5641684cc11cf7fcdce6d001
Milena <milykar13@icloud.com>
2021-01-14 18:33:11 +0000

    layout ejs added and boostrap

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const methodOverride = require ('method-override')
-const Campground = require('./models/campground')
+const engine = require('ejs-mate');
+const methodOverride = require ('method-override');
+const Campground = require('./models/campground');
 
 mongoose.connect ('mongodb://localhost:27017/yelp-camp',{
  useNewUrlParser:true,
@@ -18,7 +19,7 @@ db.once('open',() => {
 
 const app = express();
 
-
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set ('views', path.join(__dirname,'views'))
 
---
+++ middleware.js
@@ -0,0 +1,4 @@
+/*Middleware : function that runs between the request and response lifecycle 
+each middleware has acces to the request and response object and they can make 
+changes to it, can be the end of the cicle, or call the next middleware function*/
+
