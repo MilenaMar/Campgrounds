6d1d4d0b0e4b4aea167f1f94888ed8918711a116
Milena <milykar13@icloud.com>
2021-01-29 12:38:09 +0000

    basic auth completed

--- middleware.js
+++ middleware.js
@@ -4,6 +4,7 @@ changes to it, can be the end of the cicle, or call the next middleware function
 
 module.exports.isLoggedIn = (req, res, next) =>{
  if(!req.isAuthenticated()){
+     req.session.returnTo = req.originalUrl //store the url requested before login in the session
     req.flash('error', 'you must be signed in!');
     return res.redirect('/login');
 }
--- routes/user.js
+++ routes/user.js
@@ -14,8 +14,13 @@ router.post('/singup', catchAsync(async(req, res) => {
     const { email, username, password} = req.body
     const user = await new User({email, username});
     const newUser = await User.register(user, password);
-    req.flash('success', 'Welcome to Compgrounds!');
-    res.redirect('/campgrounds')
+    req.login(newUser, err => {
+        if(err){
+            return next(err)
+        }
+        req.flash('success', 'Welcome to Compgrounds!');
+        res.redirect('/campgrounds')
+    })
     } catch (e){
         req.flash('error', e.message)
         res.redirect('/singup')
@@ -28,7 +33,9 @@ router.get('/login',  (req, res)=> {
 
 router.post('/login',passport.authenticate('local', {failureFlash:true, failureRedirect:'/login'}), (req, res)=> {
 req.flash('success', "Welcome Back");
-res.redirect('/campgrounds');
+ const redirectUrl = req.session.returnTo || '/campgrounds';
+ delete req.session.returnTo;
+res.redirect(redirectUrl);
 })
 
 router.get('/logout',isLoggedIn, (req, res) => {
