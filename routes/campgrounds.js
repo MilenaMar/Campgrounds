a4f902f2b30b17c5c62604c053887c1e3e4f969d
Milena <milykar13@icloud.com>
2021-02-01 14:41:16 +0000

    router.route implemented

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,27 +4,22 @@ const router = express.Router();
 const catchAsync = require ('../utilities/catchAsync');
 const {isLoggedIn, validateCamp, isAuthor  } = require ('../middleware')
 const campgrounds = require('../controllers/campgrounds')
-// route to show all the campgrounds
+
 
 router.get ('/', catchAsync(campgrounds.index))
    
-// route to get the form for adding a new campground
-router.get ('/new', isLoggedIn, campgrounds.newForm)
-   
- // route to create the new campground  
-router.post('/new', isLoggedIn, validateCamp, catchAsync(campgrounds.newCreate))
+router.route('/new')
+.get (isLoggedIn, campgrounds.newForm)
+.post(isLoggedIn, validateCamp, catchAsync(campgrounds.newCreate))
 
-// route to get all the information for a single campground 
-router.get('/:id', catchAsync(campgrounds.singleCampground))
+router.route('/:id')
+.get(catchAsync(campgrounds.singleCampground))
+.put(isLoggedIn, isAuthor, validateCamp, catchAsync(campgrounds.editSingleCampground))
+.delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteSingleCampground))
+
    
-// route to retrive the form to edit selected campground
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.editSingleCampgroundForm))
 
-// route to edit selected campground
-router.put('/:id', isLoggedIn, isAuthor, validateCamp, catchAsync(campgrounds.editSingleCampground))
-
-// route to delete a campground 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteSingleCampground))
 
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,47 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utilities/catchAsync');
-const User = require('../models/user');
 const passport = require('passport');
 const { isLoggedIn } = require('../middleware');
+const users = require('../controllers/users')
 
-router.get('/singup', (req, res) => {
-    res.render('users/singup')
-})
+router.route('/singup')
+  .get(users.singUpForm )
+  .post(catchAsync(users.singUp))
 
-router.post('/singup', catchAsync(async(req, res) => {
-    try {
-    const { email, username, password} = req.body
-    const user = await new User({email, username});
-    const newUser = await User.register(user, password);
-    req.login(newUser, err => {
-        if(err){
-            return next(err)
-        }
-        req.flash('success', 'Welcome to Compgrounds!');
-        res.redirect('/campgrounds')
-    })
-    } catch (e){
-        req.flash('error', e.message)
-        res.redirect('/singup')
-    }  
-}))
+router.route('/login')
+   .get( users.loginForm)
+   .post(passport.authenticate
+         ('local', {failureFlash:true, 
+          failureRedirect:'/login'}), users.login)
 
-router.get('/login',  (req, res)=> {
-    res.render('users/login')
-})
 
-router.post('/login',passport.authenticate('local', {failureFlash:true, failureRedirect:'/login'}), (req, res)=> {
-req.flash('success', "Welcome Back");
- const redirectUrl = req.session.returnTo || '/campgrounds';
- delete req.session.returnTo;
-res.redirect(redirectUrl);
-})
-
-router.get('/logout',isLoggedIn, (req, res) => {
-    req.logout();
-    req.flash('success', "See you soon");
-    res.redirect('/campgrounds');
-})
+router.get('/logout',isLoggedIn, users.logout)
 
 module.exports = router;
